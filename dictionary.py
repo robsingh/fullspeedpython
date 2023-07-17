06e1f75ae40e513c02ddad20552b4f1cced991f7
robsingh <robie10895@gmail.com>
2023-07-17 16:53:07 -0700

    New statement

--- dictionary.py
+++ dictionary.py
@@ -5,7 +5,7 @@ If a key k is both in d1 and d2, the corresponding values will be added and incl
 the k and the corresponding value will be included in m
 
 """
-
+"""
 def dict_merge_sum(d1:dict,d2:dict) -> dict:
     merged_dict = {}
 
@@ -16,11 +16,62 @@ def dict_merge_sum(d1:dict,d2:dict) -> dict:
         merged_dict[key] = merged_dict.get(key, 0) + int(value)
 
     return merged_dict
-    
 
-
+    # another way
+    merged_sum = d1.copy()
+    for key,value in d2.items():
+        if key in d1:
+            d1[key] += value
+        else:
+            d1[key] = value
+    return merged_sum
+   
 
 d1 = {'a':'1','b':'2','c':'3'}
 d2 = {'a':'4','d':'5','e':'6'}
 merged_sum = dict_merge_sum(d1,d2)
 print(merged_sum)
+"""
+"""
+Exercise 2
+Given is the following simplified data of a supermarket:
+
+supermarket = { "milk": {"quantity": 20, "price": 1.19},
+               "biscuits":  {"quantity": 32, "price": 1.45},
+               "butter":  {"quantity": 20, "price": 2.29},
+               "cheese":  {"quantity": 15, "price": 1.90},
+               "bread":  {"quantity": 15, "price": 2.59},
+               "cookies":  {"quantity": 20, "price": 4.99},
+               "yogurt": {"quantity": 18, "price": 3.65},
+               "apples":  {"quantity": 35, "price": 3.15},
+               "oranges":  {"quantity": 40, "price": 0.99},
+               "bananas": {"quantity": 23, "price": 1.29}}
+
+To be ready for an imminent crisis you decide to buy everything. This isn't particularly social behavior, 
+but for the sake of the task, let's imagine it. The question is how much will you have to pay?
+"""
+
+"""def total_price(supermarket):
+    total_value = 0
+    for value in supermarket:
+        quantity = supermarket[value]["quantity"]
+        price = supermarket[value]["price"]
+        total_value += quantity * price
+    return total_value
+
+supermarket =  {
+    "milk": {"quantity": 20, "price": 1.19},
+    "biscuits":  {"quantity": 32, "price": 1.45},
+    "butter":  {"quantity": 20, "price": 2.29},
+    "cheese":  {"quantity": 15, "price": 1.90},
+    "bread":  {"quantity": 15, "price": 2.59},
+    "cookies":  {"quantity": 20, "price": 4.99},
+    "yogurt": {"quantity": 18, "price": 3.65},
+    "apples":  {"quantity": 35, "price": 3.15},
+    "oranges":  {"quantity": 40, "price": 0.99},
+    "bananas": {"quantity": 23, "price": 1.29}
+    }
+
+print(total_price(supermarket))
+"""
+
